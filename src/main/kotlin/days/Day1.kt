3ac08d201e88747546537029983f912f640630f6
Sebastian Hans <Hans.Sebastian@swm.de>
2021-12-01 15:55:16 +0100

    solve day 1, puzzle 2

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -9,3 +9,11 @@ fun solve1_1(input: String) = input.trimmedLines.map(String::toInt).let {
         acc, depth -> Pair(if (depth > acc.second) acc.first + 1 else acc.first, depth)
     }.first
 }
+
+fun solve1_2(input: String): Int {
+    val depths = input.trimmedLines.map(String::toInt)
+    return depths.subList(0, depths.size - 3)
+        .zip(depths.subList(3, depths.size))
+        .filter { it.second > it.first }
+        .size
+}
