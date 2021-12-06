92c833a0cda7981f29953d82a2dabd069a4e0e17
Sebastian Hans <Hans.Sebastian@swm.de>
2021-12-06 09:31:35 +0100

    solve day 6

---
+++ src/main/kotlin/days/Day6.kt
@@ -0,0 +1,36 @@
+package days
+
+import asCommaSeparatedInts
+
+fun solve6_1(input: String) = LanternfishPopulation.of(input.asCommaSeparatedInts)
+    .passDays(80)
+    .size
+
+fun solve6_2(input: String) = LanternfishPopulation.of(input.asCommaSeparatedInts)
+    .passDays(256)
+    .size
+
+class LanternfishPopulation(val fishesPerTimer: LongArray) {
+    companion object {
+        fun of(initialTimers: List<Int>) =
+            LanternfishPopulation(LongArray(9).apply { initialTimers.forEach { this[it]++ } })
+    }
+
+    fun passDay() = LanternfishPopulation(
+        longArrayOf(
+            fishesPerTimer[1],
+            fishesPerTimer[2],
+            fishesPerTimer[3],
+            fishesPerTimer[4],
+            fishesPerTimer[5],
+            fishesPerTimer[6],
+            fishesPerTimer[7] + fishesPerTimer[0],
+            fishesPerTimer[8],
+            fishesPerTimer[0]
+        )
+    )
+
+    fun passDays(n: Long) = (1..n).fold(this) { population, _ -> population.passDay() }
+
+    val size get() = fishesPerTimer.sum()
+}
