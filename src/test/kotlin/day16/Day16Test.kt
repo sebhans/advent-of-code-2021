72fe08c13485ead4d1899eaf37585d2a892d3d2d
Sebastian Hans <ich@sebastian-hans.de>
2021-12-19 19:40:19 +0100

    remove unused value

--- src/test/kotlin/day16/Day16Test.kt
+++ src/test/kotlin/day16/Day16Test.kt
@@ -4,16 +4,6 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class Day16Test {
-    private val input = """1163751742
-                           1381373672
-                           2136511328
-                           3694931569
-                           7463417111
-                           1319128137
-                           1359912421
-                           3125421639
-                           1293138521
-                           2311944581"""
 
     @Test
     fun `decoding the BITS structure works - example 1`() =
